48783e94926e4627ef5b5d247f729f179bf5d406
Mieszko Malawski <mmalawski@gog.com>
2019-03-25 20:08:33 +0100

    recipe model

---
+++ example_mvc/Models/MvcRecipeContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace example_mvc.Models
+{
+    public class MvcRecipeContext : DbContext
+    {
+        public MvcRecipeContext(DbContextOptions<MvcRecipeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<example_mvc.Models.Recipe> Recipe { get; set; }
+    }
+}
---
+++ example_mvc/Models/Recipe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace example_mvc.Models
+{
+    public class Recipe
+    {
+        private int Id { get; set; }
+        private string name { get; set; }
+        private string description { get; set; }
+        private string imageUrl { get; set; }
+        private string preparationTime { get; set; }
+        private enum difficulties { Easy, Medium, Hard}
+        private difficulties difficulty { get; set; }
+
+        public Recipe()
+        {
+        }
+    }
+}
