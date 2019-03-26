abf6485411bb2f4ae219753e02c3c8b8ca3a676b
JanMalawski <janmalawski2@gmail.com>
2019-03-26 18:44:44 +0100

    Scaffolded a Recipe model.

---
+++ example_mvc/Data/example_mvcContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace example_mvc.Models
+{
+    public class example_mvcContext : DbContext
+    {
+        public example_mvcContext (DbContextOptions<example_mvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<example_mvc.Models.Recipe> Recipe { get; set; }
+    }
+}
--- example_mvc/Models/Recipe.cs
+++ example_mvc/Models/Recipe.cs
@@ -3,13 +3,13 @@ namespace example_mvc.Models
 {
     public class Recipe
     {
-        private int Id { get; set; }
-        private string name { get; set; }
-        private string description { get; set; }
-        private string imageUrl { get; set; }
-        private string preparationTime { get; set; }
-        private enum difficulties { Easy, Medium, Hard}
-        private difficulties difficulty { get; set; }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+        public string PreparationTime { get; set; }
+        public enum Difficulties { Easy, Medium, Hard}
+        public Difficulties difficulty { get; set; }
 
         public Recipe()
         {
