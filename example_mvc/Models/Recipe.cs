7aea91b19e59db4c17d2ef6145377d90d64904bb
JanMalawski <janmalawski2@gmail.com>
2019-03-26 20:46:09 +0100

    Temporary comments in Recipe.cs.

--- example_mvc/Models/Recipe.cs
+++ example_mvc/Models/Recipe.cs
@@ -3,13 +3,18 @@ namespace example_mvc.Models
 {
     public class Recipe
     {
+        /// <summary>
+        /// Corrected names of fields to first capital letters.
+        /// Switched from private to public because scaffolding needed access to primary key. 
+        /// Possible problems with security in the future?
+        /// </summary>
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string ImageUrl { get; set; }
         public string PreparationTime { get; set; }
-        public enum Difficulties { Easy, Medium, Hard}
-        public Difficulties difficulty { get; set; }
+        public enum Difficulties { Easy, Medium, Hard };
+        public Difficulties Difficulty { get; set; }
 
         public Recipe()
         {
